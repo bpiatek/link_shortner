18536d721084c4da82f723318908c9527f69618a
B. Piatek <piatekbart@gmail.com>
2019-10-15 21:58:33 +0200

    remove tests for jenkins

--- src/test/java/pl/bpiatek/linkshortner/LinkShortenerApplicationTests.java
+++ src/test/java/pl/bpiatek/linkshortner/LinkShortenerApplicationTests.java
@@ -1,6 +1,5 @@
 package pl.bpiatek.linkshortner;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,26 +11,5 @@ public class LinkShortenerApplicationTests {
 
   @Test
   public void contextLoads() {
-  }
-
-  @Test
-  public void assertTrue() {
-    Assert.assertTrue(true);
-  }
-
-
-  @Test
-  public void fail() {
-    Assert.assertEquals(1, 2);
-  }
-
-  @Test
-  public void fail2() {
-    Assert.assertEquals(1, 2);
-  }
-
-  @Test
-  public void fail23() {
-    Assert.assertEquals(1, 2);
   }
 }
