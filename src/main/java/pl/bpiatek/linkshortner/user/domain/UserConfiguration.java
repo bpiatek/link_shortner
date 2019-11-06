c764c56ad276572a119fab3a218c346978c4d196
B. Piatek <piatekbart@gmail.com>
2019-11-06 15:11:56 +0100

    begin adding spring security

---
+++ src/main/java/pl/bpiatek/linkshortner/user/domain/UserConfiguration.java
@@ -0,0 +1,12 @@
+package pl.bpiatek.linkshortner.user.domain;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Bartosz Piatek on 04/11/2019
+ */
+@Configuration
+class UserConfiguration {
+
+
+}
