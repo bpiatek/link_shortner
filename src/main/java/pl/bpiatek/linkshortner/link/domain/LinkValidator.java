1c7ff2c76eb926559618b9de723d584d3422fc2a
B. Piatek <piatekbart@gmail.com>
2019-11-13 16:17:49 +0100

    refactor endpoints

--- src/main/java/pl/bpiatek/linkshortner/link/domain/LinkValidator.java
+++ src/main/java/pl/bpiatek/linkshortner/link/domain/LinkValidator.java
@@ -1,11 +1,16 @@
 package pl.bpiatek.linkshortner.link.domain;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.validator.routines.UrlValidator;
+
+import java.util.List;
 
 /**
  * Created by Bartosz Piatek on 16/08/2019
  */
 class LinkValidator {
+
+  private static final String[] REPLACE = {"", "", "", ""};
 
   boolean isValid(String link) {
     UrlValidator urlValidator = new UrlValidator() {
@@ -16,5 +21,9 @@ class LinkValidator {
     };
 
     return urlValidator.isValid(link);
+  }
+
+  String trimShortLink(String shortUrl, List<String> stringList) {
+    return StringUtils.replaceEach(shortUrl, stringList.toArray(String[]::new), REPLACE);
   }
 }
